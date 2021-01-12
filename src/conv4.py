2ae62fe70a43ba18146f80599c4b21f4860042c3
iazt <ignacio.azt@gmail.com>
2021-01-12 13:53:59 -0300

    dropout conv4

--- src/conv4.py
+++ src/conv4.py
@@ -19,16 +19,24 @@ class BaselineBB(nn.Module):
     self.fc1 = nn.Linear(1600, self.nclasses)
     self.softmax = nn.Softmax()
     self.flatten = nn.Flatten()
+    self.dropout1 = nn.Dropout()    
+    self.dropout2 = nn.Dropout()    
+    self.dropout3 = nn.Dropout()    
+    self.dropout4 = nn.Dropout()
 
      
   def forward(self, x):
     x = self.bn1(self.relu(self.conv1(x)))
     x = self.maxpool(x)
+    x = self.dropout1(x)    
     x = self.bn2(self.relu(self.conv2(x)))
     x = self.maxpool(x)
+    x = self.dropout2(x)    
     x = self.bn3(self.relu(self.conv3(x)))
     x = self.maxpool(x)
+    x = self.dropout3(x)    
     x = self.bn4(self.relu(self.conv4(x)))
     x = self.maxpool(x)
+    x = self.dropout4(x)    
     x = self.fc1(self.flatten(x))
     return x
